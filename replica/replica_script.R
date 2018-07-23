2d27cef1b507391a9ff63f2eeb7aa017cfbad737
donniemeyer32085 <123XYZabc#>
2018-07-22 21:50:05 -0700

    script

---
+++ replica/replica_script.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+
+### SEDS DATA SET ###
+
+#setwd
+setwd("~/Desktop/Springboard/data_sets_capstone/seeds/")
+## Import Data ##
+
+seeds <- read_csv("seeds_ii_replica.csv")
+glimpse(seeds)
+seeds
+
