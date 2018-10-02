33165c0139663719a2f9b34e8e7251cbb9616f5f
donniemeyer32085 <123XYZabc#>
2018-10-01 19:04:22 -0700

    push

--- replica/EDA_script.R
+++ replica/EDA_script.R
@@ -1,58 +1,27 @@
 library(tidyverse)
+library(corrplot)
 setwd("/home/donniemeyer32085/git/capstone/replica/")
 
 ### EDA ###
 
 #import data
-tidy_df <- read_csv("tidy_df_ca.csv")
+tidy_df <- read_csv("tidy_df.csv")
 glimpse(tidy_df)
 
+#create new variable for electricity consumption in kwhh
+tidy_df <- tidy_df %>% 
+  mutate(kwh_annual_usage = avg_monthly_consumption_kwh*12) %>% 
+  mutate(kwh_in_mwh_annual = kwh_annual_usage/1000) %>% 
+  mutate(diff_solargen_elec = mwh - kwh_in_mwh_annual) %>% 
+  select(geoid:cust_cnt, diff_solargen_elec, kwh_in_mwh_annual, kwh_annual_usage, everything())
+glimpse(tidy_df)
+
+sum(tidy_df$diff_solargen_elec < 0)
+
+
 #take a randow sample to work with smaller data set
-df_sample <- tidy_df[sample(1:nrow(ca_df), 64300, replace=FALSE),]
+df_sample <- tidy_df[sample(1:nrow(tidy_df), 64300, replace=FALSE),]
 
-###########################################################################bars and histograms
-df_sample %>% 
-  ggplot(aes(x = suitable_buildings, fill = income_group)) +
-  geom_histogram() +
-  xlim(c(0, 1000)) +
-  ylim(c(0, 2000))
-
-df_sample %>% 
-  ggplot(aes(x = income_group, y = suitable_buildings, fill = rent_own)) +
-  geom_bar(stat = "identity", position = position_dodge()) +
-  facet_grid(.~housing_type)
-
-
-################################################################################ scatter_plots
-
-filter(df_sample, rent_own == "own") %>% 
-  ggplot(aes(x = hh_med_income, y = suitable_buildings, color = rent_own)) +
-  geom_point(alpha = 0.1) 
-
-filter(df_sample, rent_own == "rent" & income_group == "verylow" & housing_type == "sf") %>% 
-  ggplot(aes(x = solar_gen_MWh , y = suitable_buildings, color = climate_zone_description)) +
-  geom_point(alpha = 0.1) +
-  facet_wrap(~locale)
-
-df_sample %>% 
-  ggplot(aes(x = income_group, y = dev_roof_plns, color = "factor(climate_zone)")) +
-  geom_jitter(alpha = 0.1)
-
-df_sample %>% 
-  ggplot(aes(y = suitable_buildings, x = fam_med_income)) +
-  geom_point(alpha = 0.1) +
-  facet_wrap(.~ rent_own) 
-
-vl_sf_r <- filter(df_sample, income_group == "verylow")
-vl_sf_r %>% 
-  ggplot(aes(x = number_hh, y = suitable_buildings)) +
-  geom_jitter(alpha = 0.1) +
-  geom_smooth(method = "lm") +
-  facet_wrap(rent_own ~ housing_type) +
-  scale_x_log10() +
-  scale_y_log10()
-
-########################################################################################boxplots
 
 
 
--- replica/data_wrangle_import.R
+++ replica/data_wrangle_import.R
@@ -35,51 +35,12 @@ df$variable <- str_replace(df$variable, "hh_NA", "hh")
 df$variable <- str_replace(df$variable, "mw_NA", "mw")
 df$variable <- str_replace(df$variable, "mwh_NA", "mwh")
 
-#gather hh size
-df <- gather(df, hh_size, cnt_hh_size, hh_size_1:hh_size_4)
-
-#hh_size count 
-df$hh_size <- str_replace(df$hh_size, "hh_size_1", "1")
-df$hh_size <- str_replace(df$hh_size, "hh_size_2", "2")
-df$hh_size <- str_replace(df$hh_size, "hh_size_3", "3")
-df$hh_size <- str_replace(df$hh_size, "hh_size_4", "4")
-
 #spread variable column
 df <- spread(df, variable, value)
 
-#rename
-df <- rename(df, income_group = income, 
-             number_hh = hh, 
-             suitable_buildings = bldg_cnt, 
-             dev_roof_plns = devp_cnt,
-             solar_capacity_mw = mw,
-             solar_gen_MWh = mwh,
-             avg_hh_elec_exp = elep_hh,
-             heating_d_days = hdd,
-             cooling_d_days = cdd
-)
-
 #rearange
-df <- select(df, c(geoid:lon), c(income_group:solar_gen_MWh), everything())
-df <- select(df, c(geoid:lon), income_group, housing_type, rent_own, number_hh, suitable_buildings, dev_roof_plns, devp_m2, solar_capacity_mw, solar_gen_MWh, everything())
-
-#arrange rows and select vars of interest
 df <- df %>% 
-  select(c(geoid:centroid_x), lat, lon,  c(income_group:avg_hh_elec_exp), everything()) %>% 
-  select(         -avg_ibi_pct,
-                  -hu_own,
-                  -hu_rent,
-                  -c(company_na:cust_cnt), 
-                  -hh_gini_index,
-                  -c(pop_male:hu_monthly_owner_costs_greaterthan_1000dlrs),
-                  -c(hu_med_val:aqi_median_description),
-                  -c(total_units:pct_eli_hh),
-                  
-)
-
-
-
-
+  select(geoid:centroid_x, lat:mwh, everything())
 
 ### Remove Uneeded Data ###
 write_csv(df, "tidy_df.csv")
